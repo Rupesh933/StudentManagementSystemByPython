2a79aed0b2edf5020ba6bbb52c54e9dd41ca5b90
Rupesh Rana <rupeshrana20274@gmail.com>
2026-04-22 13:17:58 +0530

    add menu for select option

--- studentManagementSystem.py
+++ studentManagementSystem.py
@@ -38,27 +38,47 @@ initialize_database()
 
 # function for generate hashable password
 import sys
-import msvcrt
+
+if os.name == 'nt':
+    import msvcrt
+
+    def _getch():
+        return msvcrt.getwch()
+else:
+    import tty
+    import termios
+
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            return sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
 def input_password(prompt='Enter password'):
     print(prompt, end='', flush=True)  # end for prevent new line and flush for immediately show data
-    
+
     password = ''
 
     while True:
-        char = msvcrt.getwch()
-        if char in ['\r', '\n']:
+        char = _getch()
+        if char in ('\r', '\n'):
             print()
             break
-        elif char == '\b':  # \b means backspace
+        elif char == '\x03':  # Ctrl+C
+            raise KeyboardInterrupt
+        elif char in ('\b', '\x7f'):  # backspace (Windows \b, Unix DEL)
             if password:
                 sys.stdout.write('\b \b')
                 sys.stdout.flush()
-            password = password[:-1]
+                password = password[:-1]
         else:
             sys.stdout.write("*")
             sys.stdout.flush()
-            password += char 
-    return password 
+            password += char
+    return password
 
 
 def create_admin_if_not_exits():
@@ -77,8 +97,62 @@ def create_admin_if_not_exits():
         print('\nAdmin Create Successfully')
         conn.close()
 
+def admin_login():
+    conn = sqlite3.connect('students.db')
+    cursor = conn.cursor()
+    username = input("Enter userName: ")
+    password = input('Enter Password: ')
+    
+    cursor.execute('SELECT * FROM admin WHERE username=? AND password=?',(username, password))
+    admin = cursor.fetchone()
+    conn.close()
+
+    if admin:
+        print('\nLogin Successful!\n')
+        return True
+    else:
+        print('\nInvalid Credentails! Try again\n')
+        return False
+    
+# menu
+def show_menu():
+    while True:
+        print('\n', '='*50)
+        print('          MAIN MENU          ')
+        print("="*50)
+        print('1. Add Student')
+        print('2. View Student')
+        print('3. Update Student')
+        print('4. Delete Student')
+        print('5. Logout/Quit')
+        print("="*50)
+
+        choice = input('Enter your choice(1-5): ')
+        if choice == '1':
+            add_student()
+        elif choice == '2':
+            view_student()
+        elif choice == '3':
+            update_student()
+        elif choice == '4':
+            delete_student()
+        elif choice == '5':
+            print('\nLogining Out Successfully!!')
+        else:
+            print('\nInvalid Choice! Please Enter Valid Option..!!')
+
+def add_student():
+    pass
+def view_student():
+    pass
+def update_student():
+    pass
+def delete_student():
+    pass
+
 def main():
     clear_screen()
+    show_menu()
 
 if __name__ == '__main__':
     main()
